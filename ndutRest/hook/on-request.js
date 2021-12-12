781d2f59fe6de84cba4ac7afb5161b6ad53f12d1
Ardhi Lukianto <ardhi@lukianto.com>
2021-12-12 20:32:46 +0700

    Use wrapped fn

--- ndutRest/hook/on-request.js
+++ ndutRest/hook/on-request.js
@@ -1,7 +1,6 @@
 module.exports = async function (request, reply) {
   const hostname = request.hostname.split(':')[0]
-  const model = this.ndutDb.model.SiteInfo
-  const rec = await model.findOne({ where: { hostname, status: 'ENABLED' } })
+  const rec = await this.ndutDb.findOne('SiteInfo', request, { where: { hostname, status: 'ENABLED' } })
   if (!rec) throw new Error('Unknown site or site is currently disabled')
   request.site = rec
 }
--- ndutRest/hook/on-response.js
+++
@@ -1,3 +0,0 @@
-module.exports = async function (request, reply) {
-  console.log('on response from ndutSite')
-}
