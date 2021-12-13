e7dd147b32ab56eea51ea04bbfd1c8ddb9e68143
Ardhi Lukianto <ardhi@lukianto.com>
2021-12-13 08:08:07 +0700

    Interceptor

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const plugin = require('./lib/plugin')
+
 module.exports = async function (fastify) {
   const name = 'ndut-site'
   const dependency = ['ndut-db']
-  return { name, dependency }
+  return { name, plugin, dependency }
 }
