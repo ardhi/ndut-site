c40ccc6b07edfe76f316c4bb3f7a96eda7d72feb
Ardhi Lukianto <ardhi@lukianto.com>
2022-01-19 15:20:48 +0700

    Updates

--- ndutDb/interceptor/find-by-id.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (model, request, ...args) {
-  if (this.ndutSite.helper.isSiteSupportedModel(model))
-    args[0] = { where: { siteId: request.site.id, id: request.params.id } }
-  else
-    args[0] = { where: { id: request.params.id } }
-  return args
-}
