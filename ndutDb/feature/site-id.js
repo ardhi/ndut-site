97705b6aa5a362f9e7fcd6f87c97a7322e91320b
Ardhi Lukianto <ardhi@lukianto.com>
2022-05-01 11:05:56 +0700

    Proper index

--- ndutDb/feature/site-id.js
+++ ndutDb/feature/site-id.js
@@ -3,7 +3,6 @@ module.exports = async function ({ builder, model, schema, options }) {
     builder.defineProperty(schema.name, 'siteId', {
       type: Number,
       required: false,
-      index: true,
       default: 0,
       scale: 0
     })
