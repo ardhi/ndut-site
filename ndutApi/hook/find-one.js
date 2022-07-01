49f9c65fcd5e474f719117b095f1d7d0827fb53f
Ardhi Lukianto <ardhi@lukianto.com>
2022-07-01 07:11:14 +0700

    Updates

--- ndutApi/hook/find-one.js
+++ ndutApi/hook/find-one.js
@@ -1,5 +1,7 @@
 module.exports = async function beforeFindOneSite ({ model, params = {}, filter = {} }) {
+  const { getNdutConfig } = this.ndut.helper
+  const cfg = getNdutConfig('ndut-site')
   const supported = await this.ndutSite.helper.isSiteSupportedModel(model)
   params.where = params.where || {}
-  if (supported && (filter.site || {}).id) params.where.siteId = filter.site.id
+  if (!cfg.singleSite && supported && (filter.site || {}).id) params.where.siteId = filter.site.id
 }
