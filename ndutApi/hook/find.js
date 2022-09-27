0c8ebcaf477f1ec4c410876830193bb51b9778cd
Ardhi Lukianto <ardhi@lukianto.com>
2022-09-27 19:12:06 +0700

    Updates

--- ndutApi/hook/find.js
+++ ndutApi/hook/find.js
@@ -1,5 +1,5 @@
 module.exports = async function beforeFindSite ({ model, params = {}, filter = {} }) {
   const supported = await this.ndutSite.helper.isSiteSupportedModel(model)
   params.where = params.where || {}
-  if (supported && (filter.site || {}).id) params.where.siteId = filter.site.id
+  if (supported && (filter.site || {}).id) filter.orWhere.push({ siteId: filter.site.id }, { siteId: { eq: null } })
 }
